9e9bc5809ed219bf4227fe95f0741e3821982984
fiddletwo <328152338+fiddletwo@users.noreply.github.com>
2026-09-16 17:36:35 -0400

    make crops harvestable

--- scripts/farming/crop_aging.gd
+++ scripts/farming/crop_aging.gd
@@ -2,14 +2,21 @@ class_name CropAging
 extends Node
 
 signal age_changed(new_age: float, last_age: float)
+signal max_growth
 
-@export var age_threshold: float = 0.2
+var harvestable: bool = false
+
+@export var age_threshold: float = 1.0
 @export var current_age: float = 0.0 :
     set(value):
         if current_age != value:
             var last_age = current_age
             current_age = value
             age_changed.emit(current_age, last_age)
+            
+        if current_age >= age_threshold and not harvestable:
+            harvestable = true
+            max_growth.emit()
             
 const group_name: String = "CropAging"
 
