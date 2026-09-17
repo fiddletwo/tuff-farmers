9fc6c2ae7a601c280a8e63bdadfff2956d6d6a01
Bruhlord274 <65053482+Hanks274@users.noreply.github.com>
2026-09-17 17:19:20 -0400

    UI Sample

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,11 +5,11 @@ extends CharacterBody2D
 @export var DRAG := 20
 
 func _physics_process(dt: float) -> void:
-    var input := Input.get_vector("left", "right", "up", "down")
-    
-    if input.length() > 0:
-        velocity = VectorUtil.expDecay(velocity, input * SPEED, ACCELERATION, dt)
-    else:
-        velocity = VectorUtil.expDecay(velocity, Vector2.ZERO, DRAG, dt)
-    
-    move_and_slide()
+	var input := Input.get_vector("left", "right", "up", "down")
+	
+	if input.length() > 0:
+		velocity = VectorUtil.expDecay(velocity, input * SPEED, ACCELERATION, dt)
+	else:
+		velocity = VectorUtil.expDecay(velocity, Vector2.ZERO, DRAG, dt)
+	
+	move_and_slide()
