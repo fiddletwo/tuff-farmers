318e7289e0aaf7d900b02f18e9ca12110db91d91
fiddletwo <328152338+fiddletwo@users.noreply.github.com>
2026-09-17 21:04:46 -0400

    menu screen ui (#1)
    * UI Sample
    
    * updated assets (deleted png)
    
    ---------
    
    Co-authored-by: Bruhlord274 <65053482+Hanks274@users.noreply.github.com>

--- scripts/update_day_night.gd
+++ scripts/update_day_night.gd
@@ -5,12 +5,12 @@ var DAY_COLOR := Color.WHITE
 var NIGHT_COLOR := Color("8668c1ff")
 
 func _ready() -> void:
-    DayNightCycle.update.connect(_on_update)
-    
+	DayNightCycle.update.connect(_on_update)
+	
 func _on_update(isDay: bool) -> void:
-    var tween := create_tween()
-    
-    if isDay:
-        tween.tween_property(self, "color", DAY_COLOR, TRANSITION_TIME)
-    else:
-        tween.tween_property(self, "color", NIGHT_COLOR, TRANSITION_TIME)
+	var tween := create_tween()
+	
+	if isDay:
+		tween.tween_property(self, "color", DAY_COLOR, TRANSITION_TIME)
+	else:
+		tween.tween_property(self, "color", NIGHT_COLOR, TRANSITION_TIME)
