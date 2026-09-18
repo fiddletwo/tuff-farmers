71aec4c85e3489a7cc2fc35e900ed46e405d6620
fiddletwo <328152338+fiddletwo@users.noreply.github.com>
2026-09-17 21:27:39 -0400

    set main screen to menu screen

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,19 +1,7 @@
 extends Control
 
+func _on_start_pressed() -> void:
+    get_tree().change_scene_to_file("res://scenes/world.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
-
-func _on_start_pressed():
-	get_tree().change_scene_to_file("res://scenes/world.tscn")
-
-
-func _on_exit_pressed():
-	get_tree().quit()
+func _on_exit_pressed() -> void:
+    get_tree().quit()
